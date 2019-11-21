db4cea2561e6c15553066db8d355702e414120cd
surister <surister98@gmail.com>
2019-11-21 10:17:29 +0100

    new stuff

--- dinos.py
+++ dinos.py
@@ -1,12 +1,14 @@
 from typing import List
+from exceptions import IncorrectSexError, WrongDinoTypeError
 
 
 class Dino:
+    # TODO type should be class containers class <(Rex)>.
     def __init__(self,
                  name: str,
-                 identifier: str,
                  level: int,
                  sex: str,
+                 dino_type: str,
                  health: int = None,
                  stamina: int = None,
                  oxygen: int = None,
@@ -18,11 +20,12 @@ class Dino:
                  impr: int = None):
 
         if sex not in ('male', 'female'):
-            print(f'Error: Sex can only be male or female on {self.id}')
+            raise IncorrectSexError(identifier=name)
 
-        self.name = name
-        self.id = identifier
+        self.name = name.lower()
         self.level = level
+        self.sex = sex
+        self.dino_type = dino_type
 
         self.health = health
         self.stamina = stamina
@@ -34,15 +37,44 @@ class Dino:
         self.torpidity = torp
         self.imprinting = impr
 
+    def __hash__(self):
+        return hash((self.name, self.sex, self.level, self.dino_type))
+
+    def __eq__(self, other):
+        if isinstance(other, Dino):
+            return hash(self) == hash(other)
+
 
 class DinoGroup:
-
-    def __init__(self, dinos: List[Dino]):
+    def __init__(self, name, dinos: List[Dino], dino_type=None):
         self._dinos = dinos
+        self.name = name
+        self.dino_type = dino_type
 
     @property
     def dinos(self):
         return self._dinos
 
+    def get_dino_by_attribute(self, attr, attr_value):
+        for dino in self._dinos:
+            if getattr(dino, attr) == attr_value:
+                return dino
+
     def __len__(self):
         return len(self.dinos)
+
+    def __str__(self):
+        return f'{self.__class__.__name__} {self.name} composed mainly by: {self.dino_type}'
+
+
+class DinoGroupMonoType(DinoGroup):
+
+    def __init__(self, name, dinos, dino_type):
+        super().__init__(name, dinos, dino_type=dino_type)
+        self.group_type = self.dinos[0].dino_type  # First dino of the group decides the group type
+        self._assert_dinotype()
+
+    def _assert_dinotype(self):
+        for dino in self.dinos:
+            if dino.dino_type != self.group_type:
+                raise WrongDinoTypeError(expected=self.group_type, expresion=dino.dino_type)
